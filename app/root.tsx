be3ca83b8175ae02d95f7a180e9b7a965a527a64
Roland Ayala <roland.ayala@gmail.com>
2023-03-31 15:31:34 -0700

    +repro

--- app/root.tsx
+++ app/root.tsx
@@ -6,11 +6,32 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
 import { ClerkApp } from "@clerk/remix";
 import type { LoaderFunction } from "@remix-run/node";
 import { rootAuthLoader } from "@clerk/remix/ssr.server";
-export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+
+export function AppCatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
 function App() {
   return (
     <html lang="en">
@@ -29,5 +50,7 @@ function App() {
     </html>
   );
 }
+// export default App; // restore this line and comment out below to disable Clerk
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
 export default ClerkApp(App);
-export const CatchBoundary = ClerkCatchBoundary();
+export const CatchBoundary = ClerkCatchBoundary(AppCatchBoundary);
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,38 +1,20 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { useState } from "react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
+const Control = () => {
+  return <div>Hello World!</div>;
+};
 
 export default function Index() {
+  const [show, isShow] = useState<boolean>(false);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <button onClick={() => isShow(!show)}>Toggle</button>
+      {show && <Control />}
     </div>
   );
 }
